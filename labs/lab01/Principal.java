f03263fbcb87ad5e70d18074e9225ec15bc46c6a
Rubens Dias <rubens.dias08@hotmail.com>
2025-02-11 12:05:22 -0300

    lab01

---
+++ labs/lab01/Principal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int x;
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		System.out.println("Valor de x =" + x + "!");
+	}
+}
---
+++ labs/lab01/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    
+}
